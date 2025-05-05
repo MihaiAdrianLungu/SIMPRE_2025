4a6c8cef9960df3c500e236a3e65acfb4074ad65
Lungu Mihai Adrian <adrian.lungu@csie.ase.ro>
2025-05-05 23:23:42 +0300

    crud methods

---
+++ utils/functions.js
@@ -0,0 +1,7 @@
+import { connectToDatabase } from "../lib/mongodb"
+
+export const getCollection = async (collectionName) => {
+    const { database } = await connectToDatabase();
+
+    return database.collection(collectionName);
+}
